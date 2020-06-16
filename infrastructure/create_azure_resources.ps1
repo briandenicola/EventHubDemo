d948e69c9d1fcdb8b8e9f56906170beb7f05acf6
Brian Denicola <bjd145@yahoo.com>
2020-06-16 08:57:35 -0500

    Simplfy deployment

--- infrastructure/create_azure_resources.ps1
+++ infrastructure/create_azure_resources.ps1
@@ -27,11 +27,12 @@ Write-Log -LogText ("Creating Resource Group for {0}" -f $appName)
 if((az group exists -g $ResourceGroup) -eq $false) {
   az group create -n $ResourceGroup -l $Region
 }
-
-$functionAppName = "func{0}01" -f $appName
-$funcStorageName = "{0}sa001" -f $functionAppName
-$eventHubNameSpace = "hub{0}01" -f $appName
-$eventHubName = "sinewave"
+$currentLocation    = $PWD.Path
+$functionAppName    = "func{0}01" -f $appName
+$funcStorageName    = "{0}sa001" -f $functionAppName
+$eventHubNameSpace  = "hub{0}01" -f $appName
+$jobName            = "stream{0}01" -f $appName
+$eventHubName       = "sinewave"
 
 Write-Log -LogText "Creating Event Hub Namespace"
 az eventhubs namespace create -g $ResourceGroup -n $eventHubNameSpace -l $region --sku Standard --enable-auto-inflate --maximum-throughput-units 5 --enable-kafka
@@ -46,10 +47,21 @@ Write-Log -LogText "Creating Azure Function"
 az functionapp create --name $functionAppName --storage-account $funcStorageName --consumption-plan-location $Region  --os-type linux --resource-group $ResourceGroup  --functions-version 3  --runtime python  --runtime-version 3.8
 az functionapp identity assign --name $functionAppName --resource-group $ResourceGroup
 
-Write-Log -LogText "Creating Azure Function"
+Write-Log -LogText "Setting up Azure Function Configuration"
 $ehConnectionString=(az eventhubs namespace authorization-rule keys list -g $ResourceGroup --namespace-name $eventHubNameSpace --name RootManageSharedAccessKey -o tsv --query primaryConnectionString)
 az functionapp config appsettings set -g $ResourceGroup -n $functionAppName --settings EVENT_HUB_NAMESPACE=$ehConnectionString
 az functionapp config appsettings set -g $ResourceGroup -n $functionAppName --settings EVENT_HUB=$eventHubName
+
+Write-Log -LogText "Deploying Function Code"
+Set-Location -Path ..\src\sine-generator
+func azure functionapp publish $functionAppName
+Set-Location -Path $currentLocation
+
+Write-Log -LogText "Deploying Stream Analytics"
+Set-Location -Path ..\src\stream-analytic-jobs
+$SubscriptionName=(az account show -o tsv --query name)
+.\Create-Streamjob.ps1 -SubscriptionName $SubscriptionName -ResourceGroup $ResourceGroup -JobName $jobName -Region $Region -EventHubNamespace $eventHubNameSpace
+Set-Location -Path $currentLocation
 
 # echo Application name
 Write-Log -LogText "------------------------------------"
