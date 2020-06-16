d62f75d5279a896037f4c178e3e1d7251fe227f5
Brian Denicola <bjd145@yahoo.com>
2020-06-16 08:39:26 -0500

    Added Stream Creation Job to deploy script

--- infrastructure/deploy_application.ps1
+++ infrastructure/deploy_application.ps1
@@ -7,4 +7,10 @@ $currentLocation = $PWD.Path
 
 Set-Location -Path ..\src\sine-generator
 func azure functionapp publish $functionAppName
+
+Set-Location -Path $currentLocation
+
+Set-Location -Path ..\src\stream-analytic-jobs
+.\Create-Streamjob.ps1
+
 Set-Location -Path $currentLocation
